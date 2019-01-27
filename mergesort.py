adc1b1f255a898fc46e79b96e1513f27c7843a39
amotz taub tabib <amotz.tt@gmail.com>
2019-01-27 11:26:31 +0200

    changing the name of the functions for clarity

--- mergesort.py
+++ mergesort.py
@@ -1,7 +1,7 @@
 import math
 
-#[amotz] this suppose to be the function that split recursively the array, i think this is working.
-def amotzmergesort(list):
+#[amotz] this suppose to be the function that split recursively the array (list in python), i think this is working.
+def listsplitter(list):
     n = len(list)
     if n < 2:
         return list
@@ -17,14 +17,19 @@ def amotzmergesort(list):
         if (mid).is_integer() == True:
             for i in range(0, int(math.ceil((n - mid)))):
                 listr[i] = list[int(math.ceil((n / 2) + i))]
-    amotzmergesort(listl)
+    listsplitter(listl)
     print(listl)
-    amotzmergesort(listr)
+    listsplitter(listr)
     print(listr)
+listsplitter ([3,6,7,8,5,2,2)
 
 
 
-#[amotz] this is supposed to be the function that merge the fragments, it is still not working
+#[amotz] this is supposed to be the function that will merge the fragments to a sorted list so in the end i will call it from my listsplitter function,
+# it is still not working though,
+#the algorithm is getting confused i think because i delete each iteration of the loop an index in the list so it says
+# an error list index out of range in line 39
+# but i am not sure yet and it is work in progress
 def mergefragments(listl, listr):
     count = 0
     length_listl = len(listl)
